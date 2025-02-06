4674df0cda4f82fe6c5a95ee2d0feb29e3bd971e
wuluj <wulujason085@gmail.com>
2025-02-07 00:27:50 +0800

    串接ckeditor5+顏色選擇棄

--- src/app/components/myarticles/myarticles.component.ts
+++ src/app/components/myarticles/myarticles.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Component, OnInit } from '@angular/core';
 import { SocialmediaService } from 'src/app/services/socialmedia.service';
+import { loadCKEditorCloud, CKEditorModule, type CKEditorCloudResult, type CKEditorCloudConfig } from '@ckeditor/ckeditor5-angular';
+
+import type { ClassicEditor, EditorConfig } from 'https://cdn.ckeditor.com/typings/ckeditor5.d.ts';
+const LICENSE_KEY =
+  'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDAwOTU5OTksImp0aSI6IjAyNDhiMTFhLTU0ZDQtNDIzZi04NTFmLWEyYTA2ODIzY2FiZCIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImEwOWU3ZDIwIn0.vxr1VsfKg7W4Q58SL66gRKE3eqcERkRaMXA4AZyywVzwS9vx0O6WLlIkuNrWFTBn1Q34TeRofuRdm-Z1mDRlqw';
+const cloudConfig = {
+  version: '44.1.0'
+} satisfies CKEditorCloudConfig;
 
 @Component({
   selector: 'app-myarticles',
@@ -8,16 +15,15 @@ import { SocialmediaService } from 'src/app/services/socialmedia.service';
   styleUrls: ['./myarticles.component.css']
 })
 export class MyarticlesComponent {
-  Editor = ClassicEditor;
-  editorData = '<p>這是 CKEditor 內容</p>';
-  editorConfig = {
-    licenseKey: 'GPL'
-  };
+  Editor: typeof ClassicEditor | null = null;
+  config: EditorConfig | null = null;
+  editorData = '';
   datas = [];
   imageData: { [key: number]: string[] } = {};
   constructor(private socialmediaService: SocialmediaService) { };
   ngOnInit(): void {
     this.get();
+    loadCKEditorCloud(cloudConfig).then(this._setupEditor.bind(this));
   }
   loadImages(postId: number) {
     this.socialmediaService.getMyImages(postId).subscribe(data => {
@@ -33,4 +39,136 @@ export class MyarticlesComponent {
       });
     })
   }
+  //
+  private _setupEditor(cloud: CKEditorCloudResult<typeof cloudConfig>) {
+    const {
+      ClassicEditor,
+      Alignment,
+      Autosave,
+      Bold,
+      Essentials,
+      FontBackgroundColor,
+      FontColor,
+      FontFamily,
+      FontSize,
+      Heading,
+      Highlight,
+      HorizontalLine,
+      Indent,
+      IndentBlock,
+      Italic,
+      List,
+      Paragraph,
+      Strikethrough,
+      TodoList,
+      Underline
+    } = cloud.CKEditor;
+
+    this.Editor = ClassicEditor;
+    this.config = {
+      toolbar: {
+        items: [
+          'heading',
+          '|',
+          'fontSize',
+          'fontFamily',
+          'fontColor',
+          'fontBackgroundColor',
+          '|',
+          'bold',
+          'italic',
+          'underline',
+          'strikethrough',
+          '|',
+          'horizontalLine',
+          'highlight',
+          '|',
+          'alignment',
+          '|',
+          'bulletedList',
+          'numberedList',
+          'todoList',
+          'outdent',
+          'indent'
+        ],
+        shouldNotGroupWhenFull: false
+      },
+      plugins: [
+        Alignment,
+        Autosave,
+        Bold,
+        Essentials,
+        FontBackgroundColor,
+        FontColor,
+        FontFamily,
+        FontSize,
+        Heading,
+        Highlight,
+        HorizontalLine,
+        Indent,
+        IndentBlock,
+        Italic,
+        List,
+        Paragraph,
+        Strikethrough,
+        TodoList,
+        Underline
+      ],
+      fontFamily: {
+        supportAllValues: true
+      },
+      fontSize: {
+        options: [10, 12, 14, 'default', 18, 20, 22],
+        supportAllValues: true
+      },
+      heading: {
+        options: [
+          {
+            model: 'paragraph',
+            title: 'Paragraph',
+            class: 'ck-heading_paragraph'
+          },
+          {
+            model: 'heading1',
+            view: 'h1',
+            title: 'Heading 1',
+            class: 'ck-heading_heading1'
+          },
+          {
+            model: 'heading2',
+            view: 'h2',
+            title: 'Heading 2',
+            class: 'ck-heading_heading2'
+          },
+          {
+            model: 'heading3',
+            view: 'h3',
+            title: 'Heading 3',
+            class: 'ck-heading_heading3'
+          },
+          {
+            model: 'heading4',
+            view: 'h4',
+            title: 'Heading 4',
+            class: 'ck-heading_heading4'
+          },
+          {
+            model: 'heading5',
+            view: 'h5',
+            title: 'Heading 5',
+            class: 'ck-heading_heading5'
+          },
+          {
+            model: 'heading6',
+            view: 'h6',
+            title: 'Heading 6',
+            class: 'ck-heading_heading6'
+          }
+        ]
+      },
+      licenseKey: LICENSE_KEY,
+      placeholder: '請輸入內容!'
+    };
+  }
+  //
 }
