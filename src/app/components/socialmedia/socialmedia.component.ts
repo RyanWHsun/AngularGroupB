0aab9e22be63954c41007467381d6c0f220652d8
wuluj <wulujason085@gmail.com>
2025-02-17 03:37:08 +0800

    取得文章的userNickName,img

--- src/app/components/socialmedia/socialmedia.component.ts
+++ src/app/components/socialmedia/socialmedia.component.ts
@@ -1,6 +1,7 @@
 import { Component, SimpleChanges } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { SocialmediaService } from 'src/app/services/socialmedia.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-socialmedia',
@@ -10,12 +11,13 @@ import { SocialmediaService } from 'src/app/services/socialmedia.service';
 export class SocialmediaComponent {
   datas: any[] = [];
   imageData: { [key: number]: string[] } = {};
+  userData: { [key: number]: { image: string, nickName: string } } = {};
   activePostIds: number[] = [];
   page: number = 1;
-  pageSize: number = 2;
+  pageSize: number = 3;
   loading: boolean = false;
   hasMore: boolean = true;
-  constructor(private socialmediaService: SocialmediaService) { };
+  constructor(private socialmediaService: SocialmediaService, private userService: UserService) { };
   ngOnInit(): void {
     this.loadArticles();
   }
@@ -24,15 +26,28 @@ export class SocialmediaComponent {
       this.imageData[postId] = data.map((imageBase64: string) => 'data:image/jpeg;base64,' + imageBase64);
     })
   }
+  loadUserInfo(userId: number) {
+    if (!this.userData[userId]) {
+      this.userData[userId] = { image: '', nickName: '' };
+    }
+    this.userService.getUser(userId).subscribe(data => {
+      this.userData[userId] = {
+        image: `data:image/jpeg;base64,${data.fUserImage}`,
+        nickName: data.fUserNickName
+      }
+    })
+  }
   loadArticles() {
-    if (!this.hasMore || this.loading) return;  // 如果沒有更多文章或正在載入則不執行
+    if (!this.hasMore || this.loading) return;
 
     this.loading = true;
     this.socialmediaService.getPublicArticles(this.page, this.pageSize).subscribe(response => {
       if (response.length > 0) {
         this.datas.push(...response);
+        // console.log(this.datas);
         this.datas.forEach(post => {
           this.loadImages(post['fPostId']);
+          this.loadUserInfo(post['fUserId']);
         });
         this.page++;
       } else {
