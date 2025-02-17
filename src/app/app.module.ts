a6e10912b5082c3e816b28e2f422ce29377308f1
雨塵 <jer1357900@gmail.com>
2025-02-17 12:14:58 +0800

    刪除活動報名 改進詳細資料裡

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,6 @@ import { PaymentComponent } from './components/payment/payment.component';
 import { ProductPipe } from './pipes/product.pipe';
 import { AttractionTicketComponent } from './components/attraction-ticket/attraction-ticket.component';
 import { AttractionAdminComponent } from './components/attraction-admin/attraction-admin.component';
-import { EventRegistrationComponent } from './pages/event-registration/event-registration.component';
 
 @NgModule({
   declarations: [
@@ -64,7 +63,6 @@ import { EventRegistrationComponent } from './pages/event-registration/event-reg
     ProductPipe,
     AttractionTicketComponent,
     AttractionAdminComponent,
-    EventRegistrationComponent,
   ],
   imports: [
     BrowserModule,
